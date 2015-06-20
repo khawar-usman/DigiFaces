850baedab39a4357e9023698e8ebc9340a0e7d12
shahid-nasrullah-confiz <shahid.nasrullah@confiz.com>
2015-06-20 20:40:47 +0500

    Views updated

--- DigiFaces/AppDelegate.m
+++ DigiFaces/AppDelegate.m
@@ -18,6 +18,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
+    [[UINavigationBar appearance] setBarTintColor:[UIColor blackColor]];
+    [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor colorWithRed:38/255.0f green:218/255.0f blue:1 alpha:1]}];
+    
     
     NSString * userNameSaved = [[NSUserDefaults standardUserDefaults]objectForKey:@"userName"]?[[NSUserDefaults standardUserDefaults]objectForKey:@"userName"]:@"";
     
